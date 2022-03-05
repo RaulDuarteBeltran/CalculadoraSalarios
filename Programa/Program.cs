0296da1fcb1533abd8d7cf9feecc8ed5c2c2e67f
RaulDuarteeltran <raul_duarteb@hotmail.com>
2022-03-05 16:46:59 -0600

    Catch errors on main

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -4,6 +4,7 @@ using Common.Interfaces;
 using Common.Utility;
 using Common.AbstractClasses;
 using Common.Entities;
+using Common.Exceptions;
 using Programa.Prototype1.Modelo.Utility;
 
 namespace Programa
@@ -12,25 +13,40 @@ namespace Programa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, please give me the name of the file you want to use");
-            string fileName = Console.ReadLine();
-
-            IParserBuilder parserBuilder = new NaiveBuilder();
-            ISourceParser sourceParser = parserBuilder.Build();
-
-            //Obtener que archivo quiero utilizar
-            IList<Employee> employeeCollection = sourceParser.Parse(fileName);
-
-            //Calculamos el salario y lo imprimimos
-            ISalaryCalculator salaryCalculator = new SalaryCalculator();
-            foreach (Employee employee in employeeCollection)
+            try
             {
-                double salary = salaryCalculator.CalculateSalary(employee);
-                Console.WriteLine($"The amount to pay {employee.Name} is:{salary} USD");
-            }
+                Console.WriteLine("Hi, please give me the name of the file you want to use");
+                string fileName = Console.ReadLine();
 
-            Console.WriteLine("End of the execution, have a nice day");
-            Console.ReadKey();
+                IParserBuilder parserBuilder = new NaiveBuilder();
+                ISourceParser sourceParser = parserBuilder.Build();
+
+                //Obtener que archivo quiero utilizar
+                IList<Employee> employeeCollection = sourceParser.Parse(fileName);
+
+                //Calculamos el salario y lo imprimimos
+                ISalaryCalculator salaryCalculator = new SalaryCalculator();
+                foreach (Employee employee in employeeCollection)
+                {
+                    double salary = salaryCalculator.CalculateSalary(employee);
+                    Console.WriteLine($"The amount to pay {employee.Name} is:{salary} USD");
+                }
+
+                Console.WriteLine("End of the execution, have a nice day");
+                Console.ReadKey();
+            }
+            catch(FileValidationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
